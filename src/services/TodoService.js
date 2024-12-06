cf99fd393b3872b2cce9070e6c814c79011745cf
rahul <rahul@192.168.1.6>
2024-12-07 01:35:53 +0530

    frontend

---
+++ src/services/TodoService.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const BASE_REST_API_URL = import.meta.env.VITE_API_URL
+  ? import.meta.env.VITE_API_URL
+  : "http://localhost:8080/api/todos"; // Default to localhost if not defined
+
+// export function getAllTodos(){
+//     return axios.get(BASE_REST_API_URL);
+// }
+
+export const getAllTodos = () => axios.get(BASE_REST_API_URL);
+
+export const saveTodo = (todo) => axios.post(BASE_REST_API_URL, todo);
+
+export const getTodo = (id) => axios.get(BASE_REST_API_URL + "/" + id);
+
+export const updateTodo = (id, todo) =>
+  axios.put(BASE_REST_API_URL + "/" + id, todo);
+
+export const deleteTodo = (id) => axios.delete(BASE_REST_API_URL + "/" + id);
+
+export const completeTodo = (id) =>
+  axios.patch(BASE_REST_API_URL + "/" + id + "/complete");
+
+export const inCompleteTodo = (id) =>
+  axios.patch(BASE_REST_API_URL + "/" + id + "/in-complete");
